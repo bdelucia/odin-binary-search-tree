0a7c5584967c8de574214085b33ca85649af1ca0
bdelucia <bdelucia@asu.edu>
2025-01-26 15:45:29 -0700

    find delete add

--- bst.js
+++ bst.js
@@ -44,4 +44,54 @@ export function Tree() {
   this.insert = function (value) {
     this.addNode(value);
   };
+
+  this.deleteItem = function (value) {
+    this.root = deleteRec(this.root, value);
+
+    function deleteRec(root, value) {
+      if (root === null) {
+        return root;
+      }
+
+      if (value < root.data) {
+        root.left = deleteRec(root.left, value);
+      } else if (value > root.data) {
+        root.right = deleteRec(root.right, value);
+      } else {
+        if (root.left === null) {
+          return root.right;
+        } else if (root.right === null) {
+          return root.left;
+        }
+
+        root.data = minValue(root.right);
+        root.right = deleteRec(root.right, root.data);
+      }
+
+      return root;
+    }
+
+    function minValue(node) {
+      let minv = node.data;
+      while (node.left !== null) {
+        minv = node.left.data;
+        node = node.left;
+      }
+      return minv;
+    }
+  };
+  this.find = function (value) {
+    let current = this.root;
+    while (current) {
+      if (value === current.data) {
+        return current;
+      }
+      if (value < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return null;
+  };
 }
--- main.js
+++ main.js
@@ -6,3 +6,5 @@ tree.buildTree([5, 3, 8, 2, 4, 7, 9]);
 printTree(tree);
 tree.insert(6);
 printTree(tree);
+tree.deleteItem(8);
+printTree(tree);
